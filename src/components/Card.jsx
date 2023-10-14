5fdf939185d245af232d5464d3f394687ac0b259
V C <107280002+VictorC1707@users.noreply.github.com>
2023-10-14 17:07:05 -0400

    estilos

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,38 +1,19 @@
 export default function Card(props) {
    return (
-        <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+        <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3 ">
 
-            
-            <article class="overflow-hidden rounded-lg shadow-lg">
-
-                <a href="#">
-                    <img alt="Placeholder" class="block h-auto w-full"  src={props.image}/>
-                </a>
-
-                <header class="flex items-center justify-between leading-tight p-2 md:p-4">
-                    <h1 class="text-lg">
-                        <a class="no-underline hover:underline text-black" href="#">
-                        {props.name}
-                        </a>
-                    </h1>
-                    <p class="text-grey-darker text-sm">
-                    {props.species}
-                    </p>
-                </header>
-
-                <footer class="flex items-center justify-between leading-none p-2 md:p-4">
-                    <p class="flex items-center no-underline  text-black" href="#">
-                        <p class="ml-2 text-sm">
-                        {props.gender}
-                        </p>
-                    </p>
-                    <a onClick={props.onClose} class="no-underline text-grey-darker hover:text-red-dark" href="#">
-                        <span class="hidden">Like</span>
-                        X
-                    </a>
-                </footer>
-
-            </article>
+<div class="group before:hover:scale-95 before:hover:h-72 before:hover:w-80 before:hover:h-44 before:hover:rounded-b-2xl before:transition-all before:duration-500 before:content-[''] before:w-80 before:h-24 before:rounded-t-2xl bg-slate-600 before:bg-gradient-to-bl from-sky-200 via-green-400 to-green-700 before:absolute before:top-0 w-80 h-72 relative bg-slate-50 flex flex-col items-center justify-center gap-2 text-center rounded-2xl overflow-hidden">
+<div class="flex justify-end w-full">
+                            <a onClick={props.onClose} class=" pe-10 cursor-pointer z-10 h-6 w-6 text-white fill-current">
+                               X
+                            </a>
+                        </div>
+  <img class="w-28 h-28 bg-blue-700 mt-8 rounded-full border-4 border-slate-50 z-10 group-hover:scale-100 group-hover:-translate-x-24  group-hover:-translate-y-20 transition-all duration-500" src={props.image}></img>
+  <div class="z-10  group-hover:-translate-y-20 transition-all duration-500">
+    <span class="text-2xl font-semibold">{props.name}</span>
+    <p>{props.species}</p>
+  </div>
+</div>
             
 
         </div>
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,20 @@
+import "./SearchBar.css"
+
 export default function SearchBar(props) {
    return (
-      <div>
-          <input type='search' />
-      <button onClick={props.onSearch}>Agregar</button> 
-      </div>
+      
+      
+  <div class="relative p-12 w-full sm:max-w-2xl sm:mx-auto">
+    <div class="overflow-hidden z-0 rounded-full relative p-3">
+      <form role="form" class="relative flex z-50 bg-transparent rounded-full">
+        <input type="input" placeholder="enter your search here" class="rounded-full flex-1 px-6 py-4 text-gray-700 focus:outline-none"/>
+        <button onClick={props.onSearch} class="bg-indigo-500 text-white rounded-full font-semibold px-8 py-4 hover:bg-indigo-400 focus:bg-indigo-600 focus:outline-none">Search</button>
+      </form>
+      <div class="glow glow-1 z-10 bg-green-300 absolute"></div>
+      <div class="glow glow-2 z-20 bg-green-400 absolute"></div>
+      <div class="glow glow-3 z-30 bg-green-800 absolute"></div>
+      <div class="glow glow-4 z-40 bg-green-600 absolute"></div>
+    </div>
+  </div>
    );
 }
